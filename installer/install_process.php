b77e8352bb605bffc96df81fc9ff8cf99e9e9770
elvioladordemark <168953399+elvioladordemark@users.noreply.github.com>
2024-06-19 22:11:36 -0400

    Update install_process.php

--- installer/install_process.php
+++ installer/install_process.php
@@ -5,7 +5,7 @@
     <title>Instalador</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
-<body class="bg-gray-100">
+<body class="bg-gray-100"
     <div class="min-h-screen flex items-center justify-center">
         <div class="bg-white p-8 rounded shadow-md w-96">
 <?php
